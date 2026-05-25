00da9dff8d8a6c2bbcd63549cb92de561a385ca9
Mallconx14 <mallconx14@gmail.com>
2026-05-25 10:15:57 -0300

    Endereço das rotas criados em server.js

--- server.js
+++ server.js
@@ -13,6 +13,19 @@ mongoose.connection.on('connected', () => {
     console.log('Connected to MongoDB');
 });
 
+const musicasRoutes = require('./routes/musicas');
+const filmesRoutes = require('./routes/filmes');
+const jogosRoutes = require('./routes/jogos');
+const livrosRoutes = require('./routes/livros');
+const seriesRoutes = require('./routes/series');
+
+app.use('/musicas', musicasRoutes);
+app.use('/filmes', filmesRoutes);
+app.use('/jogos', jogosRoutes);
+app.use('/livros', livrosRoutes);
+app.use('/series', seriesRoutes);
+
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
 });
+
