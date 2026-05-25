3cda143a4669c7cdda17aa12eb4c7f29ac441a53
Mallconx14 <mallconx14@gmail.com>
2026-05-25 10:51:31 -0300

    Rota get das musicas

--- routes/musicas.js
+++ routes/musicas.js
@@ -2,6 +2,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 
+const musicaSchema = new mongoose.Schema({
+    titulo: String,
+    artista: String,
+    genero: String,
+    ano: Number,
+    duracao: Number,
+    disponivel: Boolean,
+    detalhes: Object
+});
 
+const Musica = mongoose.model('musicas', musicaSchema);
+
+router.get('/', async(req, res) => {
+    try {
+        const musicas = await Musica.find();
+        res.status(200).json(musicas);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
 
 module.exports = router
